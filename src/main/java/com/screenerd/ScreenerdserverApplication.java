99555f213703f63565a53ef7564fddd958900be4
telly <teliwal91@gmail.com>
2018-02-22 12:27:19 +0100

    init gradle

---
+++ src/main/java/com/screenerd/ScreenerdserverApplication.java
@@ -0,0 +1,12 @@
+package com.screenerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenerdserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenerdserverApplication.class, args);
+	}
+}
