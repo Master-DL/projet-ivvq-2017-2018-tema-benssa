fb32abbbd870e8735790f3aec10cbfe71b523266
FontBenjamin <font.benjamin@yahoo.fr>
2018-03-30 09:50:47 +0200

    test is now commented

--- src/test/groovy/com/screenerd/GrooSpec.groovy
+++ src/test/groovy/com/screenerd/GrooSpec.groovy
@@ -4,11 +4,11 @@ import spock.lang.Specification
 
 /**
  * Created by telly on 09/03/18.
- */
+
 
 class GrooSpec extends Specification{
     def "let's try this!"() {
         expect:
         Math.max(1, 2) == 3
-    }
+    } */
 }
