459ad121366372cab278a31e62bb3ff4c78de3e2
FontBenjamin <font.benjamin@yahoo.fr>
2018-04-06 14:46:10 +0200

    added comment groovy test

--- src/main/java/com/screenerd/domain/Comment.java
+++ src/main/java/com/screenerd/domain/Comment.java
@@ -22,5 +22,39 @@ public class Comment {
     @ManyToOne
     private  Post post;
 
-    public Comment(){}
+    public Comment(String content, User user, Post post){
+        this.content = content;
+        this.user = user;
+        this.post = post;
+    }
+    
+    public Long getId(){
+    	return id;
+    }
+    
+    public String getContent(){
+    	return content;
+    }
+    
+    public User getUser(){
+    	return user;
+    }
+    
+    public User getPost(){
+    	return post;
+    }
+    
+    public void setUser(User user){
+    	this.user = user;
+    }
+    
+    public void setContent(String content){
+    	this.content = content;
+    }
+    
+    public void setPost(Post post){
+    	this.post = post;
+    }
+    
 }
+
---
+++ src/test/groovy/com/screenerd/domain/CommentTest.groovy
@@ -0,0 +1,51 @@
+package com.screenerd
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import javax.validation.Validation
+import javax.validation.Validator
+import javax.validation.ValidatorFactory
+
+
+class CommentTest extends GrooSpec {
+
+    Validator validator
+
+    void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Unroll
+    void "test la validite d'un commentaire valide"(String unContent, User unUser, Post unPost) {
+
+        given: "a correctly "
+        Comment comment = new Comment(content: unContent, user: unUser, post: unPost)
+
+        expect: "the comment is valid"
+        validator.validate(comment).empty
+
+        where:
+        unContent   | unUser       | unPost
+        "commentaire"    | new User()   | new Post()
+
+    }
+
+    @Unroll
+    void "test l'invalidite d'un comentaire non valide"(String unNom, String unPrenom, String unEmail, String unSexe) {
+
+        given: "un commentaire initialise de maniere non valide"
+        Comment comment = new Comment(content: unContent, user: unUser, post: unPost)
+
+        expect: "l'utilisateur est invalide"
+        validator.validate(comment).empty
+
+        where:
+        unContent   | unUser       | unPost
+        null    | new User()   | new Post()
+        "commentaire"    | null   | new Post()
+        "commentaire"    | new User()   | null
+
+    }
+}
