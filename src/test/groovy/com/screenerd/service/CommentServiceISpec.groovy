ec199f88f0f1f7b3f34fc46fa6fd479ee54ef311
FontBenjamin <font.benjamin@yahoo.fr>
2018-05-20 17:51:06 +0200

    CommentServiceITest done

--- src/test/groovy/com/screenerd/service/CommentServiceISpec.groovy
+++ src/test/groovy/com/screenerd/service/CommentServiceISpec.groovy
@@ -1,22 +1,23 @@
 package com.screenerd.service
 
 import com.screenerd.domain.Comment
+import org.springframework.boot.test.context.SpringBootTest
+
+import javax.validation.ConstraintViolationException
 import com.screenerd.domain.Post
 import com.screenerd.domain.User
 import org.springframework.beans.factory.annotation.Autowired
 import spock.lang.Specification
-import spock.lang.Unroll
 
-import javax.validation.ConstraintViolationException
-import javax.validation.Validation
-import javax.validation.Validator
-import javax.validation.ValidatorFactory
-
-
+@SpringBootTest
 class CommentServiceISpec extends Specification {
 
     @Autowired
-    CommentService commentService;
+    private CommentService commentService
+    @Autowired
+    private UserService userService
+    @Autowired
+    private PostService postService
 
     def "test save a valid comment"(){
         given: "a user"
@@ -27,9 +28,10 @@ class CommentServiceISpec extends Specification {
         Comment comment = new Comment(content:"a comment", user: user, post: post);
 
         when: "the comment is saved"
-        if(comment == null)
-            println "Comment NULLLLLLLLL"
-        commentService.saveComment(comment)
+        User savedUser = userService.saveUser(user)
+        Post savedPost = postService.savePost(post)
+        user.getComments().add(comment)
+        Comment savedComment = commentService.saveComment(comment)
 
         then: "the comment has an id"
         comment.id != null
@@ -41,10 +43,13 @@ class CommentServiceISpec extends Specification {
         and: "a post"
         Post post = new Post(user: user, description: "GG", image: [0, 0, 0, 0, 0] as byte[] , imageFormat: "png");
         and: "a comment"
-        Comment comment = new Comment(content:"a", user: user, post: post);
+        Comment comment = new Comment(content:"a", user: null, post: post);
 
         when: "the user is saved"
-        commentService.saveComment(comment)
+        User savedUser = userService.saveUser(user)
+        Post savedPost = postService.savePost(post)
+        user.getComments().add(comment)
+        Comment savedComment = commentService.saveComment(comment)
 
         then: "A validation exception is thrown"
         thrown ConstraintViolationException
