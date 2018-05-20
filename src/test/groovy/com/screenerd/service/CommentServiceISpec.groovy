37443dd7c88acb6759687237ab8530c742be56a9
FontBenjamin <font.benjamin@yahoo.fr>
2018-05-20 17:34:29 +0200

    Continuing the debugging of the test

--- src/test/groovy/com/screenerd/service/CommentServiceISpec.groovy
+++ src/test/groovy/com/screenerd/service/CommentServiceISpec.groovy
@@ -16,20 +16,22 @@ import javax.validation.ValidatorFactory
 class CommentServiceISpec extends Specification {
 
     @Autowired
-    CommentService commentService
+    CommentService commentService;
 
     def "test save a valid comment"(){
         given: "a user"
         User user = new User(login: "login",password: "password",avatar: [1,3,6]);
         and: "a post"
-        Post post = new Post(user, "GG", [0, 0, 0, 0, 0] as byte[], "png");
+        Post post = new Post(user: user, description: "GG", image: [0, 0, 0, 0, 0] as byte[] , imageFormat: "png");
         and: "a comment"
         Comment comment = new Comment(content:"a comment", user: user, post: post);
 
         when: "the comment is saved"
+        if(comment == null)
+            println "Comment NULLLLLLLLL"
         commentService.saveComment(comment)
 
-        then: "the user has an id"
+        then: "the comment has an id"
         comment.id != null
     }
 
