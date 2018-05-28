48b460d8727965280d54e73d87d38eddb0eb3728
FontBenjamin <font.benjamin@yahoo.fr>
2018-05-28 20:59:28 +0200

    added controler

---
+++ src/main/java/com/screenerd/controller/CommentController.java
@@ -0,0 +1,38 @@
+package com.screenerd.controller;
+
+import com.screenerd.domain.Comment;
+import com.screenerd.domain.Post;
+import com.screenerd.domain.User;
+import com.screenerd.service.CommentService;
+import com.screenerd.service.PostService;
+import com.screenerd.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping(value = "/api/v1/comment", method = RequestMethod.POST)
+    public Comment addComment(@RequestParam(value = "idUser") Long idUser,
+                              @RequestParam(value = "idPost") Long idPost,
+                              @RequestParam(value = "content") String content) {
+        User user = userService.findUser(idUser);
+        Post post = postService.getPostRepository().findOne(idPost);
+        Comment toSave = new Comment(content, user, post);
+        return commentService.saveComment(toSave);
+    }
+
+    /**@RequestMapping(value = "/api/v1/deleteComment/{id}", method = RequestMethod.DELETE)
+    public void deleteComment(@PathVariable("id") Long id) {
+        commentService.deleteComment(id);
+    }*/
+}
