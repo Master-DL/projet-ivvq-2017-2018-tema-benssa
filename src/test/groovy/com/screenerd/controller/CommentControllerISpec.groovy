cd9572bb6ab4ae9c754f74d11d71f252c0b88bd3
MKostiuk <mathieukostiuk@hotmail.fr>
2018-05-31 17:11:33 +0200

    merge

--- src/test/groovy/com/screenerd/controller/CommentControllerISpec.groovy
+++ src/test/groovy/com/screenerd/controller/CommentControllerISpec.groovy
@@ -8,6 +8,7 @@ import com.screenerd.repository.CommentRepository
 import com.screenerd.repository.LikeRepository
 import com.screenerd.repository.PostRepository
 import com.screenerd.repository.UserRepository
+import com.screenerd.service.InitializationService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -24,30 +25,17 @@ class CommentControllerISpec extends Specification{
     @Autowired
     TestRestTemplate restTemplate
     @Autowired
-    UserRepository userRepository
-    @Autowired
-    PostRepository postRepository
-    @Autowired
     CommentRepository commentRepository
-
-    User user
-    Post post
-    Comment comment
-
-    def setup(){
-        user = new User(login: "login",password: "password",avatar: [1, 3, 6])
-        userRepository.save(user)
-        post = new Post(user: user,description: "Descritpion", image: [0, 0, 0, 0, 0] as byte[],  imageFormat: "png")
-        postRepository.save(post)
-    }
+    @Autowired
+    InitializationService initializationService
 
     def "test add a valid comment"(){
         when: "when the add comment URL is triggered with valid input"
         MultiValueMap<String,Object> map = new LinkedMultiValueMap<String,Object>()
-        map.add("idUser",user.id)
-        map.add("idPost",post.id)
+        map.add("idUser",initializationService.ben.id)
+        map.add("idPost",initializationService.pesByThomas.id)
         map.add("content","ceci est un commentaire")
-        comment = restTemplate.postForObject("/api/v1/comment",map,Comment.class)
+        def comment = restTemplate.postForObject("/api/v1/comment",map,Comment.class)
 
         then: "the comment is created and has correct values"
         comment.id != null
@@ -56,7 +44,8 @@ class CommentControllerISpec extends Specification{
 
     void "test delete comment"() {
         given: "a valid saved comment "
-        Comment comment = new Comment(content: "ceci est un commentaire",user: user,post: post)
+        Comment comment = new Comment(content: "ceci est un commentaire",user: initializationService.ben,
+                post: initializationService.pesByThomas)
         commentRepository.save(comment)
 
         when: "comment is deleted"
