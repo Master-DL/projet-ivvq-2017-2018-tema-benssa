390a35e0264029ad141894524684aa3596e9ab4c
sarasamouh <sarazinebsamouh@gmail.com>
2018-06-01 19:10:52 +0200

    fix  code smell

--- src/main/java/com/screenerd/controller/UserController.java
+++ src/main/java/com/screenerd/controller/UserController.java
@@ -3,13 +3,10 @@ package com.screenerd.controller;
 import com.screenerd.domain.User;
 import com.screenerd.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.Map;
+
 
 /**
  * Created by mousa on 27/04/2018.
